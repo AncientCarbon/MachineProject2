37731af410eb8b1d3ed1ea3ecf32e796d1d30c31
AncientCarbon <boblo.ludvigsen@gmail.com>
2022-04-01 14:58:18 +0200

    Doubly Linked List oprettet generateDeck metode lavet printDeck metode lavet

--- main.c
+++ main.c
@@ -1,33 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node{
-    int data;
-    struct Node* next;
-    struct Node* previous;
+void generateDeck();
+void printDeck();
+struct Card{
+    int shown;
+    int value;
+    int suit;
+    struct Card* next;
+    struct Card* previous;
+
 };
+struct Card* newCard;
+struct Card* head;
+struct Card* tail;
+struct Card* prevCard;
 
 int main() {
-    struct Node* head = NULL;
-    struct Node* second = NULL;
-    struct Node* third = NULL;
-
     // allocate 3 nodes in the heap
-    head = (struct Node*)malloc(sizeof(struct Node));
-    second = (struct Node*)malloc(sizeof(struct Node));
-    third = (struct Node*)malloc(sizeof(struct Node));
+    generateDeck();
+    printDeck();
 
-    head->data = 1;
-    head->next = second;
-    head->previous = NULL;
-
-    second->data = 2;
-    second->next = third;
-    second->previous = head;
-
-    third->data = 3;
-    third->next = NULL;
-    third->previous = second;
 
     return 0;
+}
+
+void generateDeck(){
+
+    //------------------------------------------------------------------------------------------------------------------
+    // Suit 1 = Clubs (C)
+    // Suit 2 = Diamonds (D)
+    // Suit 3 = Hearts (H)
+    // Suit 4 = Spades (S)
+    //------------------------------------------------------------------------------------------------------------------
+
+    int value = 1;
+    int suit = 1;
+
+    head = (struct Card*)malloc(sizeof(struct Card));
+    head->suit = suit;
+    head->value = value;
+    head->previous = NULL;
+    head->next = NULL;
+
+
+
+    for (int i = 0; i < 51; i++) {
+        value++;
+        if (value > 13) {
+            value = 1;
+            suit++;
+        }
+        newCard = (struct Card *) malloc(sizeof(struct Card));
+
+        if (suit == 1 && value == 2) {
+            newCard->previous = head;
+            newCard->next = NULL;
+            head->next = newCard;
+            prevCard = newCard;
+        } else {
+            newCard->previous = prevCard;
+            newCard->next = NULL;
+            prevCard->next = newCard;
+            prevCard = newCard;
+        }
+        newCard->suit = suit;
+        newCard->value = value;
+    }
+}
+
+void printDeck(){
+    newCard = head;
+    for (int i = 0; i < 52; i++){
+        switch (newCard->suit){
+            case(1): printf("%d of Clubs\n", newCard->value); break;
+            case(2): printf("%d of Diamonds\n", newCard->value); break;
+            case(3): printf("%d of Hearts\n", newCard->value); break;
+            case(4): printf("%d of Spades\n", newCard->value); break;
+        }
+        newCard = newCard->next;
+    }
 }
