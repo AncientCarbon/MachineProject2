a95f79db3ffa02cc2f9374fa0030f0510814c10a
AncientCarbon <boblo.ludvigsen@gmail.com>
2022-04-03 16:30:30 +0200

    printTable metode lavet flipCard metode lavet (virker ikke)

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 
 void generateDeck();
 void printDeck();
+void printTable();
+void flipCard();
 struct Card{
     int shown;
     int value;
@@ -13,13 +15,14 @@ struct Card{
 };
 struct Card* newCard;
 struct Card* head;
-struct Card* tail;
 struct Card* prevCard;
 
 int main() {
-    // allocate 3 nodes in the heap
     generateDeck();
+    newCard = head;
     printDeck();
+    flipCard(newCard);
+    printTable();
 
 
     return 0;
@@ -40,6 +43,7 @@ void generateDeck(){
     head = (struct Card*)malloc(sizeof(struct Card));
     head->suit = suit;
     head->value = value;
+    head->shown = 0;
     head->previous = NULL;
     head->next = NULL;
 
@@ -52,6 +56,7 @@ void generateDeck(){
             suit++;
         }
         newCard = (struct Card *) malloc(sizeof(struct Card));
+        newCard->shown = 0;
 
         if (suit == 1 && value == 2) {
             newCard->previous = head;
@@ -77,6 +82,31 @@ void printDeck(){
             case(2): printf("%d of Diamonds\n", newCard->value); break;
             case(3): printf("%d of Hearts\n", newCard->value); break;
             case(4): printf("%d of Spades\n", newCard->value); break;
+        }
+        newCard = newCard->next;
+    }
+}
+void flipCard(struct Card* current){
+    current = head;
+    if (current->shown == 0) current->shown = 1;
+    else current->shown = 0;
+}
+void printTable(){
+    newCard = head;
+    printf("C1\tC2\tC3\tC4\tC5\tC6\tC7\n");
+    int counter = 0;
+    for (int i = 0; i < 52; i++){
+        counter++;
+        if (newCard->shown == 0) printf("[]\t");
+        else switch (newCard->suit){
+            case(1): printf("%dC\t", newCard->value); break;
+            case(2): printf("%dD\t", newCard->value); break;
+            case(3): printf("%dH\t", newCard->value); break;
+            case(4): printf("%dS\t", newCard->value); break;
+        }
+        if (counter == 7){
+            printf("\n");
+            counter = 0;
         }
         newCard = newCard->next;
     }
